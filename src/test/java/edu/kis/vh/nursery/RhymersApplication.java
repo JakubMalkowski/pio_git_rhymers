862831a5e87e9585a323aa54b018b5c2a01878cb
Jakub Malkowski <malkowski2003@interia.pl>
2024-04-14 18:34:20 +0200

    4.1 poprawa nazw klas d

--- src/test/java/edu/kis/vh/nursery/RhymersApplication.java
+++ src/test/java/edu/kis/vh/nursery/RhymersApplication.java
@@ -1,17 +1,18 @@
 package edu.kis.vh.nursery;
 
-import edu.kis.vh.nursery.defaultCountingOutRhymer;
+import edu.kis.vh.nursery.DefaultCountingOutRhymer;
 import edu.kis.vh.nursery.HanoiRhymer;
 import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
 import edu.kis.vh.nursery.factory.Rhymersfactory;
 
-class RhymersDemo {
+class RhymersApplication {
 
     public static void main(String[] args) {
-        Rhymersfactory rhymersFactory = new DefaultRhymersFactory();
 
-        defaultCountingOutRhymer[] rhymers = { rhymersFactory.GetStandardRhymer(), rhymersFactory.GetFalseRhymer(),
-                rhymersFactory.GetFIFORhymer(), rhymersFactory.GetHanoiRhymer()};
+        Rhymersfactory factory = new DefaultRhymersFactory();
+
+        DefaultCountingOutRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
+                factory.GetFIFORhymer(), factory.GetHanoiRhymer()};
         
         for (int i = 1; i < 15; i++)
             for (int j = 0; j < 3; j++)
