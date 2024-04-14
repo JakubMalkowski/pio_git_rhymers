7cea2c1d9d85376da76151665502860693682e1e
Jakub Malkowski <malkowski2003@interia.pl>
2024-04-14 18:52:03 +0200

    5 poprawa nazw zmiennych

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -8,18 +8,17 @@ import edu.kis.vh.nursery.factory.Rhymersfactory;
 class RhymersDemo {
 
     public static void main(String[] args) {
-        Rhymersfactory factory = new DefaultRhymersFactory();
-        
-        defaultCountingOutRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
-                factory.GetFIFORhymer(), factory.GetHanoiRhymer()};
+        Rhymersfactory rhymersFactory = new DefaultRhymersFactory();
+
+        defaultCountingOutRhymer[] rhymers = { rhymersFactory.GetStandardRhymer(), rhymersFactory.GetFalseRhymer(),
+                rhymersFactory.GetFIFORhymer(), rhymersFactory.GetHanoiRhymer()};
         
         for (int i = 1; i < 15; i++)
             for (int j = 0; j < 3; j++)
                 rhymers[j].countIn(i);
-        
-        java.util.Random rn = new java.util.Random();
+        java.util.Random randomNumber = new java.util.Random();
         for (int i = 1; i < 15; i++)
-            rhymers[3].countIn(rn.nextInt(20));
+            rhymers[3].countIn(randomNumber.nextInt(20));
         
         for (int i = 0; i < rhymers.length; i++) {
             while (!rhymers[i].callCheck())
